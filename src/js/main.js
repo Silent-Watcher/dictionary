5efe06409e69b6eec2996ecf3c55c9bc3b8e72e0
Ali.t.nazari <alitabatabaee20@gmail.com>
2023-03-27 15:31:57 +0430

    add searchbox functionality

--- src/js/main.js
+++ src/js/main.js
@@ -2,14 +2,42 @@
 
 const $ = document;
 const defaultTitle = 'Dictionary';
+const searchBtn = $.querySelector('#search_btn');
+const searchInput = $.querySelector('#search_input');
+
+window.addEventListener('load',()=>{
+    setTimeout(() => {
+        searchInput.focus();
+    }, 2000);
+})
 
 // dynamic tab change
 window.addEventListener('blur', () => {
   $.title = 'come back 😥';
-  setTimeout(() => {$.title = defaultTitle}, 3000);
+  setTimeout(() => {
+    $.title = defaultTitle;
+  }, 3000);
 });
 window.addEventListener('focus', () => {
   $.title = defaultTitle;
 });
 // ----------------------
 
+searchBtn.addEventListener('click', handleUserRequest);
+searchInput.addEventListener('keypress', (event) => {
+  event.code === 'Enter' &&  handleUserRequest();
+});
+
+function handleUserRequest() {
+  if (searchInput.value.trim() === '') {
+    Swal.fire({
+      title: 'Error!',
+      text: 'Empty values ​​are not allowed',
+      icon: 'error',
+      confirmButtonText: 'Got it',
+    });
+  } else {
+    
+  }
+  searchInput.value = '';
+}
