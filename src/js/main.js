dda347cb2625d9bed173d35e48d42a24e2352bf6
Ali.t.nazari <alitabatabaee20@gmail.com>
2023-03-27 15:11:04 +0430

    add dynamic tab

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const $ = document;
+const defaultTitle = 'Dictionary';
+
+// dynamic tab change
+window.addEventListener('blur', () => {
+  $.title = 'come back 😥';
+  setTimeout(() => {$.title = defaultTitle}, 3000);
+});
+window.addEventListener('focus', () => {
+  $.title = defaultTitle;
+});
+// ----------------------
+
