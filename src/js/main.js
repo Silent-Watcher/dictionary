75943239b31f8ce8188fa50615acf28967137914
Ali.t.nazari <alitabatabaee20@gmail.com>
2023-03-27 19:57:35 +0430

    fetch the meanings of the word from api

--- src/js/main.js
+++ src/js/main.js
@@ -3,25 +3,23 @@ import fireAlert from './helpers/alert.js';
 import capitalize from './helpers/text.helper.js';
 
 const $ = document,
-  defaultTitle = 'Dictionary',
   searchBtn = $.querySelector('#search_btn'),
   searchInput = $.querySelector('#search_input'),
   API_URL = import.meta.env.VITE_BASE_URL,
-  audio = $.querySelector('#audio'),
-  audioBtn = $.querySelector('#audio_btn');
+  audio = $.querySelector('#audio');
 
-window.addEventListener('load', () => {
-  isAudioSrcProvided(audio);
-});
+
+const wordTemplate = $.querySelector('#word_template');
+const api_response_wrapper = $.querySelector('#api_response_wrapper');
+
+
 
 // dynamic tab change
 window.addEventListener('blur', () => {
   $.title = 'come back 😥';
-  setTimeout(() => {
-    $.title = defaultTitle;
-  }, 3000);
 });
 window.addEventListener('focus', () => {
+  const defaultTitle = 'Dictionary';
   $.title = defaultTitle;
 });
 // ----------------------
@@ -32,6 +30,7 @@ searchInput.addEventListener('keypress', (event) => {
 });
 
 async function handleUserRequest() {
+  api_response_wrapper.innerHTML = null;
   let word = searchInput.value;
   if (word.trim() === '')
     fireAlert('Error!', 'Empty values ​​are not allowed', 'error');
@@ -47,30 +46,62 @@ async function handleUserRequest() {
   }
 }
 
-audioBtn.addEventListener('click', function () {
-  if (isAudioSrcProvided(audio)) audio.play();
-  else fireAlert('Error', 'failed to load the audio file');
-});
 
 // api operations
 function sendApiRequest(word) {
   return fetch(`${API_URL}/${word}`).then((res) => {
-    if (res.status === 404) {
+    if (res.status === 200) return res.json();
+    else if (res.status === 404) {
       return new Error(`404 "${word}" not found 🤔`);
     }
-    if (res.status === 200) return res.json();
   });
 }
 
 function addApiResponse(response) {
-  console.log(response);
+ 
+  api_response_wrapper.appendChild(wordTemplate.content.cloneNode(true));
   $.querySelector('#word').innerHTML = capitalize(response.at(0).word);
   audio.src = response[0].phonetics[0].audio;
+
+  let fragment = $.createDocumentFragment();
+
+  let meanings = $.createElement('section');
+  meanings.className = "meanings row col-12 mt-4";
+
+  response[0].meanings.forEach( meaning => {
+    let card = `
+    <div class="card col-12 col-lg-6">
+      <h5 class="partOfSpeech card-header">${meaning.partOfSpeech || 'not found'}</h5>
+      <div class="card-body">
+        <div class="definitions">
+          <h5 class="definition card-title">${meaning.definitions[0].definition || 'not found'}</h5>
+          <p class="example card-text ms-3">${meaning.definitions[0]?.example ?? 'not found'}</p>
+          <ul class="list-group">
+            <li class="list-group-item d-flex justify-content-between align-items-start">
+              <div class="ms-2 me-auto">
+                <div class="fw-bold">synonyms</div>
+                <p class="synonyms ms-3">${meaning.synonyms.length ?  meaning.synonyms.join(', ') : '..'}</p>
+              </div>
+            </li>
+            <li class="list-group-item d-flex justify-content-between align-items-start">
+              <div class="ms-2 me-auto">
+                <div class="fw-boldx">antonyms</div>
+                <p class="antonyms ms-3">${meaning.antonyms.length ?  meaning.antonyms.join(', ') : '..'}</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>`;
+    meanings.insertAdjacentHTML('beforeend',card);
+  })
+    fragment.appendChild(meanings);
+    api_response_wrapper.appendChild(fragment);
 }
 // ---------------------------------------------
 
 // audio button validations
-function isAudioSrcProvided(audioElement) {
+function isAudioSrcProvided(audioElement , audioBtn) {
   if (audioElement.src === window.location.href) {
     disableActiveBtn(audioBtn);
     return false;
@@ -95,4 +126,11 @@ function disableActiveBtn(audioBtn) {
     'fa-volume-xmark'
   );
 }
+
+function playAudioBtn(event){
+  if (isAudioSrcProvided(audio , event.target.parentElement)) audio.play();
+  else fireAlert('Error', 'failed to load the audio file');
+}
 // ---------------------------------------------
+
+window.playAudioBtn = playAudioBtn;
